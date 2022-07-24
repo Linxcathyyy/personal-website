202f66aa918e806d43d60ae84ec271bb0b11b0cb
Xingchen Lin <linxingchen0428cathy@gmail.com>
2022-07-24 12:15:08 +0800

    Updates

---
+++ precache-manifest.2f2dbbcbadc0cf46826fec5839aec773.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "2d996ba7bf5d7d070194",
+    "url": "/personal-website/static/css/main.41aaecad.chunk.css"
+  },
+  {
+    "revision": "2d996ba7bf5d7d070194",
+    "url": "/personal-website/static/js/main.563880fa.chunk.js"
+  },
+  {
+    "revision": "b6d8e3a721968ec40bad",
+    "url": "/personal-website/static/js/runtime~main.93f2c6c9.js"
+  },
+  {
+    "revision": "5594b54bd2a2174f4fb6",
+    "url": "/personal-website/static/js/2.7ec30c4c.chunk.js"
+  },
+  {
+    "revision": "8d278ea3fddd6336d27842e3e44d51cf",
+    "url": "/personal-website/static/media/cv.8d278ea3.pdf"
+  },
+  {
+    "revision": "6db7d1196d4b29e994fde6c5ce310b9b",
+    "url": "/personal-website/static/media/EduSearch_Poster.6db7d119.png"
+  },
+  {
+    "revision": "13db5e2202aa39b783fded70d877b662",
+    "url": "/personal-website/static/media/portfolio2.13db5e22.jpg"
+  },
+  {
+    "revision": "9a197f583c3d00ecb639f92a0216dd8a",
+    "url": "/personal-website/static/media/Sellah.9a197f58.png"
+  },
+  {
+    "revision": "4d78181e94e3d7b5a33bec3c2b29417f",
+    "url": "/personal-website/static/media/MeowTheChatBot.4d78181e.png"
+  },
+  {
+    "revision": "f5db156bc6a1d3d41d91d5fdb9797273",
+    "url": "/personal-website/index.html"
+  }
+];
